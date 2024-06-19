fc87a0a8ecd949b5a32fddfc1a3569eda85e8bf6
PavlosPo <pavlos.poulos@outlook.com>
2024-06-19 13:45:33 +0300

    First initial upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import torch
+from transformers import AutoTokenizer, DataCollatorForSeq2Seq, T5ForConditionalGeneration, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
+from datasets import load_dataset
+import evaluate
+import numpy as np
+
+# Load the T5 model and tokenizer
+checkpoint = "google-t5/t5-small"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+
+# Dataset
+billsum = load_dataset("billsum", split="ca_test")
+billsum = billsum.train_test_split(test_size=0.2)
+train = billsum['train'] # Train Dataset 80%
+temp = billsum['test'].train_test_split(test_size=0.5)  # Ignore
+test = temp['test'] # Test Dataset
+val = temp['train'] # Val Dataset
+
+# Load evaluation
+rouge = evaluate.load("rouge")
+
+# Example text to summarize
+text = train[0]['train']
+summary = train[0]['summary']
+print(f"\nExample Text: \n{text}\nExample Summary: \n{summary}\n")
+
+
+print("Preprocessing the Dataset Phase starts...\n")
+prefix = "summarize: "  # Required so the T5 model knows that we are going to summarize
+def preprocess_function(examples):
+    inputs = [prefix + doc for doc in examples["text"]]
+    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+
+    labels = tokenizer(text_target=examples["summary"], max_length=128, truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=checkpoint)
+
+tokenized_billsum = billsum.map(preprocess_function, batched=True)
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    result = rouge.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+    result["gen_len"] = np.mean(prediction_lens)
+
+    return {k: round(v, 4) for k, v in result.items()}
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="my_awesome_billsum_model",
+    eval_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    weight_decay=0.01,
+    save_total_limit=3,
+    num_train_epochs=4,
+    predict_with_generate=True,
+    fp16=True,
+    push_to_hub=True,
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_billsum["train"],
+    eval_dataset=tokenized_billsum["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+
+# Example usage
+if __name__ == "__main__":
+    trainer.train()
