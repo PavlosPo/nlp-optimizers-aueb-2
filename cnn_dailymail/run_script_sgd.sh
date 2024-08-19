e6bb0c5dfe6c79b5d9a2754d640d1a06dd5f7a4e
PavlosPo <pavlos.poulos@outlook.com>
2024-08-20 00:35:31 +0300

    Updating the scripts in order to clean the checkpoints after each seed trials

--- cnn_dailymail/run_script_sgd.sh
+++ cnn_dailymail/run_script_sgd.sh
@@ -23,6 +23,7 @@ wait
 
 # Add a delay to let the TPU clean itself
 echo "Waiting for 10 seconds..."
+rm -rf ./checkpoints/**
 sleep 10
 
 # Run the Python script with the second set of arguments
@@ -34,6 +35,7 @@ wait
 
 # Add a delay to let the TPU clean itself
 echo "Waiting for 10 seconds..."
+rm -rf ./checkpoints/**
 sleep 10
 
 # Run the Python script with the Third set of arguments
@@ -45,6 +47,7 @@ wait
 
 # Add a delay to let the TPU clean itself
 echo "Waiting for 10 seconds..."
+rm -rf ./checkpoints/**
 sleep 10
 
 # Run the Python script with the Fourth set of arguments
@@ -56,4 +59,5 @@ wait
 
 # Add a delay to let the TPU clean itself
 echo "Waiting for 10 seconds..."
+rm -rf ./checkpoints/**
 sleep 10
