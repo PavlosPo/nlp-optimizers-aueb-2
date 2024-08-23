be5470bb31cc4bfc0c929877c5d271afd72be7b1
PavlosPo <pavlos.poulos@outlook.com>
2024-08-23 15:26:27 +0300

    Updating functionality

--- cnn_dailymail_full_training_old/parameter_explorer.py
+++ cnn_dailymail_full_training_old/parameter_explorer.py
@@ -6,11 +6,11 @@ def read_best_hyperparameters(base_path='./hypertuning_results_full_training/goo
     
     # Mapping optimizer-specific names to generic parameter names to be used in the training with the correct argument names
     param_mapping = {
-        'adam': {'beta1': 'betas[0]', 'beta2': 'betas[1]', 'epsilon': 'eps'},
-        'adamax': {'beta1': 'betas[0]', 'beta2': 'betas[1]', 'epsilon': 'eps'},
-        'adamw': {'beta1': 'betas[0]', 'beta2': 'betas[1]', 'epsilon': 'eps'},
-        'nadam': {'beta1': 'betas[0]', 'beta2': 'betas[1]', 'epsilon': 'eps', 'momentum_decay': 'momentum_decay'},
-        'sgdm': {'momentum': 'momentum'},
+        'adam': {'learning_rate': 'lr', 'beta1': 'betas[0]', 'beta2': 'betas[1]', 'epsilon': 'eps'},
+        'adamax': {'learning_rate': 'lr', 'beta1': 'betas[0]', 'beta2': 'betas[1]', 'epsilon': 'eps'},
+        'adamw': {'learning_rate': 'lr', 'beta1': 'betas[0]', 'beta2': 'betas[1]', 'epsilon': 'eps'},
+        'nadam': {'learning_rate': 'lr', 'beta1': 'betas[0]', 'beta2': 'betas[1]', 'epsilon': 'eps', 'momentum_decay': 'momentum_decay'},
+        'sgdm': {'learning_rate': 'lr', 'momentum': 'momentum'},
         # Add mappings for other optimizers if needed
     }
 
@@ -79,7 +79,7 @@ def main():
         
         for seed_dir, params in seeds_data.items():
             seed = int(seed_dir.split('_')[1])
-            learning_rate = params.get('learning_rate', None)
+            learning_rate = params.pop('lr', None)
             
             if learning_rate is None:
                 print(f"Skipping seed {seed} due to missing learning rate.")
