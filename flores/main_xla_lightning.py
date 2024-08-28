7641e7b35043a330807c1872a58445f95a1e13ee
PavlosPo <pavlos.poulos@outlook.com>
2024-08-28 13:24:43 +0300

    changed the length of the datasets

--- flores/main_xla_lightning.py
+++ flores/main_xla_lightning.py
@@ -287,18 +287,21 @@ class T5TranslationDataModule(pl.LightningDataModule):
                 dataset = load_dataset(self.dataset_name, 'all',  trust_remote_code=True)
                 temp1 = dataset['dev']
                 temp2 = dataset['devtest']
-                # concat the two splits
-                dataset = concatenate_datasets([temp1, temp2]).train_test_split(test_size=0.5, seed=self.seed_num, shuffle=True)
+                # concat the two splits, and get 80% train and 10% test and 10% validation
+                # total dataset
+                dataset = concatenate_datasets([temp1, temp2]).train_test_split(test_size=0.8, seed=self.seed_num, shuffle=True)
                 
                 if split == 'train':
                     data = dataset['train']
                 elif split == 'validation':
                     # Split again into train and test, and select train as the validation set
+                    # 10% of the total dataset is selected as the validation set
                     selected_data = dataset['test'].train_test_split(test_size=0.5, seed=self.seed_num, shuffle=True)['train']
                     data = selected_data.select(range(min(self.test_range, len(selected_data))))
                     
                 elif split == 'test':
                     # Split again into train and test, and select test as the test set
+                    # 10% of the total dataset is selected as the test set
                     selected_data = dataset['test'].train_test_split(test_size=0.5, seed=self.seed_num, shuffle=True)['test']
                     data = selected_data.select(range(min(self.test_range, len(selected_data))))
                 
