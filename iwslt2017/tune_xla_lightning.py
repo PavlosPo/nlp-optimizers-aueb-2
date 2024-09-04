65912ff280828e0cd736a1ce13b2b34e24859874
PavlosPo <pavlos.poulos@outlook.com>
2024-09-04 17:16:02 +0300

    the code now it also loads per machine

--- iwslt2017/tune_xla_lightning.py
+++ iwslt2017/tune_xla_lightning.py
@@ -159,6 +159,7 @@ class T5TranslationDataModule(pl.LightningDataModule):
         #     self.datasets[lang] = load_dataset(self.dataset_name, code, trust_remote_code=True)
         #     self.datasets[lang] = self.datasets[lang].shuffle(seed=self.seed_num)
         
+        # Loads one time to download, in one machine if in distributed training.
         self.datasets['Romanian'] = load_dataset(self.dataset_name, self.language_codes['Romanian'], trust_remote_code=True).shuffle(seed=self.seed_num)
         self.datasets['German'] = load_dataset(self.dataset_name, self.language_codes['German'], trust_remote_code=True).shuffle(seed=self.seed_num)
         self.datasets['French'] = load_dataset(self.dataset_name, self.language_codes['French'], trust_remote_code=True).shuffle(seed=self.seed_num)
@@ -189,6 +190,11 @@ class T5TranslationDataModule(pl.LightningDataModule):
                 with open(cache_file, 'rb') as f:
                     dataset = pickle.load(f)
             else:
+                # Load per machine..
+                self.datasets['Romanian'] = load_dataset(self.dataset_name, self.language_codes['Romanian'], trust_remote_code=True).shuffle(seed=self.seed_num)
+                self.datasets['German'] = load_dataset(self.dataset_name, self.language_codes['German'], trust_remote_code=True).shuffle(seed=self.seed_num)
+                self.datasets['French'] = load_dataset(self.dataset_name, self.language_codes['French'], trust_remote_code=True).shuffle(seed=self.seed_num)
+        
                 print(f"Processing {split} dataset for {language}...")                
                 if split == 'train':
                     train_dataset = self.datasets[language]['train']
